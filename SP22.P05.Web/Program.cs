22bdb8e959ad5b2bfe835f74a453fcda2fb72229
Caleb Martin <calebm776@cox.net>
2022-02-22 17:09:09 -0600

    Revert "Did some stuff with cors, may not be needed"
    This reverts commit 5b631b36488e72e26af8bce5215cf4026d73211b.

--- SP22.P05.Web/Program.cs
+++ SP22.P05.Web/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SP22.P05.Web.Data;
 using SP22.P05.Web.Features.Authorization;
-using System.Web.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
