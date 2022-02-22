2de4d9a48cea55feb8c3d8825149e6bb0d9b2030
jaredmartin5 <jaredm776@cox.net>
2022-02-22 13:57:30 -0600

    simple navbar

--- SP22.P05.Web/ClientApp/src/App.js
+++ SP22.P05.Web/ClientApp/src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./components/Home";
+import { Navmenu } from "./components/Navmenu";
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Navmenu />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
---
+++ SP22.P05.Web/ClientApp/src/components/Navmenu.js
@@ -0,0 +1,11 @@
+import { Navbar, Container  } from "react-bootstrap";
+
+export function Navmenu() {
+    return (
+    <Navbar expand="lg" variant="light" bg="light">
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+        </Container>
+    </Navbar>
+    );
+}
