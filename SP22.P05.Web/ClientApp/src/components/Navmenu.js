0cd09e2f7e2615c82a03bb02fbfdf50a42632481
Caleb Martin <calebm776@cox.net>
2022-02-26 17:40:51 -0600

    More login

--- SP22.P05.Web/ClientApp/src/components/Navmenu.js
+++ SP22.P05.Web/ClientApp/src/components/Navmenu.js
@@ -1,20 +1,25 @@
 import { Navbar, Container } from "react-bootstrap";
 import "./Navmenu.css";
 import iceLogo from '../content/ice_logo.png';
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Alert } from "react-bootstrap";
 import { useState } from "react";
 import axios from "axios";
 import { Form } from "react-bootstrap";
+import { Link } from "react-router-dom"
 
 export function Navmenu() {
 
   const [show, setShow] = useState(false);
   const [userName, setUsername] = useState("")
   const [password, setPassword] = useState("")
+  const [isLoginFail, setisLoginFail] = useState(false);
 
-  const handleClose = () => setShow(false);
+  // const handleClose = () => setShow(false);
+  function handleClose() {
+    setShow(false);
+    setisLoginFail(false);
+  }
   const handleShow = () => setShow(true);
-
 
   function handleLogin() {
     axios.post('/api/authentication/login', {
@@ -26,8 +31,20 @@ export function Navmenu() {
         handleClose()
       })
       .catch(function (error) {
+        setisLoginFail(true)
         console.log(error);
       });
+  }
+
+  function AlertPassword() {
+    if (isLoginFail) {
+      return (
+        <Alert variant="danger">
+          <Alert.Heading>Invalid Username or Password.</Alert.Heading>
+        </Alert>
+      )
+    }
+    return <></>
   }
 
   return (
@@ -46,13 +63,14 @@ export function Navmenu() {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicUsername">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Username" value={userName} onChange={(e) => setUsername(e.target.value)}/>
+              <Form.Control type="text" placeholder="Username" value={userName} onChange={(e) => setUsername(e.target.value)} />
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+              <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </Form.Group>
           </Form>
+          <AlertPassword />
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
