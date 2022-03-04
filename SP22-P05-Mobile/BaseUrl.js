718ca4bf75e12feee4e8bea8f3e950ca6c97e90b
jaredmartin5 <jaredm776@cox.net>
2022-03-04 16:05:24 -0600

    more product info

--- SP22-P05-Mobile/BaseUrl.js
+++ SP22-P05-Mobile/BaseUrl.js
@@ -1,3 +1,3 @@
 // const baseUrl = "https://selu383-sp22-p05-g01.azurewebsites.net/"
-const baseUrl = "https://69df-147-174-8-187.ngrok.io"
+const baseUrl = "http://7b19-68-105-145-219.ngrok.io"
 export default baseUrl;
