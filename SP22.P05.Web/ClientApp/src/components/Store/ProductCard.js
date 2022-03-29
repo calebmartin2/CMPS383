ef79df7412da44232a1a9ab58322cedc4294d040
Caleb Martin <calebm776@cox.net>
2022-03-29 16:41:16 -0500

    styling

--- SP22.P05.Web/ClientApp/src/components/Store/ProductCard.js
+++ SP22.P05.Web/ClientApp/src/components/Store/ProductCard.js
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 export function ProductCard({ myProduct }) {
     return (
         <Col>
-        <Link to={`/product/${myProduct.id}/${myProduct.name}`} style={{ textDecoration: 'none' }}>
-            <Card className="ProductCard h-100" bg="black" text="white" >
-                {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
-                <Card.Body>
-                    <Card.Title style={{ fontWeight: 700 }}>{myProduct.name}</Card.Title>
-                    <Card.Subtitle>{myProduct.blurb}</Card.Subtitle>
-                </Card.Body>
-                <Card.Footer>${myProduct.price.toFixed(2)}</Card.Footer>
-            </Card>
+            <Link to={`/product/${myProduct.id}/${myProduct.name}`} style={{ textDecoration: 'none' }}>
+                <Card className="ProductCard h-100" bg="black" text="white" >
+                    {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
+                    <Card.Body>
+                        <Card.Title style={{ fontWeight: 700 }}>{myProduct.name}</Card.Title>
+                        <Card.Subtitle>{myProduct.blurb}</Card.Subtitle>
+                    </Card.Body>
+                    <Card.Footer><Card.Text style={{ float: "left", color: "#999" }}>{myProduct.publisherName}</Card.Text>${myProduct.price.toFixed(2)}</Card.Footer>
+                </Card>
             </Link>
         </Col>
 
