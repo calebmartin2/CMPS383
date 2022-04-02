4fdcbd1695c1105b84be9ae7e3c11ed7d88b6586
jaredmartin5 <jaredm776@cox.net>
2022-04-02 15:32:17 -0500

    Terms of Agreement to Terms and Conditions

--- SP22.P05.Web/ClientApp/src/components/Auth/SignUp.js
+++ SP22.P05.Web/ClientApp/src/components/Auth/SignUp.js
@@ -62,7 +62,7 @@ export function SignUp() {
                     <Form.Control required type="password" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value) }  />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check required type="checkbox" label={<>I agree to the <Link to="/terms" target="_blank" rel="noopener noreferrer" style={{ color: "#84AEC8" }}>Terms of Agreement</Link></>} />
+                    <Form.Check required type="checkbox" label={<>I agree to the <Link to="/terms" target="_blank" rel="noopener noreferrer" style={{ color: "#84AEC8" }}>Terms and Conditions</Link></>} />
                 </Form.Group>
                 <Button type="submit" variant="primary" className="custom-primary-btn" style={{ marginBottom: "0.5em" }} disabled={isLoading}>
                     SIGN UP
