e4d155bb621adf5723fa13a62db82becf1eb4a86
Kevin Martin <kevin.martin-3@selu.edu>
2022-04-07 16:40:25 -0500

    fixed network error. i goofed.

--- SP22-P05-Mobile/BaseUrl.js
+++ SP22-P05-Mobile/BaseUrl.js
@@ -1,3 +1,3 @@
 const baseUrl = "https://021b-147-174-8-183.ngrok.io"
- //"https://selu383-sp22-p05-g01.azurewebsites.net/"
+//"https://selu383-sp22-p05-g01.azurewebsites.net/"
 export default baseUrl;
--- SP22-P05-Mobile/Screens/LibraryScreen.js
+++ SP22-P05-Mobile/Screens/LibraryScreen.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState, useContext } from "react";
-import {Text} from "react-native";
+import {Text, Card} from "react-native";
+import baseUrl from '../BaseUrl';
+import { Col, Row, Grid } from "react-native-easy-grid";
 import authCookieContext from '../Authorization/AuthCookieProvider';
 
 export default function UserLibrary() {
@@ -9,12 +11,13 @@ export default function UserLibrary() {
 
     useEffect(() => {
         axios({
-            url: '/api/products/library',
+            url: baseUrl + '/api/products/library',
             method: 'get',
             headers: { Cookie: authCookie }
         })
             .then(function (response) {
                 setProducts(response.data);
+                console.log(products)
             })
             .catch(function (error) {
                 console.log(error);
