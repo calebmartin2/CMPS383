13a8f18c37f17de3f91574f7312a5208e2ce204d
Caleb Martin <calebm776@cox.net>
2022-04-12 13:23:11 -0500

    Fix baseUrl

--- SP22-P05-Mobile/BaseUrl.js
+++ SP22-P05-Mobile/BaseUrl.js
@@ -1,3 +1,2 @@
-const baseUrl = "https://96c1-147-174-8-178.ngrok.io"
-//"https://selu383-sp22-p05-g01.azurewebsites.net/"
+const baseUrl = "https://selu383-sp22-p05-g01.azurewebsites.net/"
 export default baseUrl;
