1c8c9dc23b2b721dfb02b550c7a5ed5b4ccec5df
Caleb Martin <calebm776@cox.net>
2022-04-16 20:05:14 -0500

    Clean UserProductController

--- SP22.P05.Web/Controllers/UserProductController.cs
+++ SP22.P05.Web/Controllers/UserProductController.cs
@@ -29,29 +29,25 @@ public class UserProductController : Controller
     {
         var products = dataContext.Set<Product>();
         var totalAmount = products.Where(x => productId.Contains(x.Id)).Sum(x => x.Price);
-
-        int? userId = User.GetCurrentUserId();
-        if (userId == null)
-        {
-            return BadRequest("User invalid, doesn't exist?");
-        }
-        ICollection<ProductUser> addList = new List<ProductUser>() { };
-        Order order = new Order()
+        int userId = Convert.ToInt32(User.GetCurrentUserId());
+        var addList = new List<ProductUser>() { };
+        var order = new Order()
         {
             UserId = (int)userId,
             Amount = totalAmount,
             Date = DateTimeOffset.Now,
         };
+
         foreach (int id in productId)
         {
             var product = products.FirstOrDefault(x => x.Id == id);
+
             if (product == null)
-            {
                 return BadRequest();
-            }
+
             addList.Add(new ProductUser
             {
-                UserId = (int)userId,
+                UserId = userId,
                 ProductId = id,
                 Order = order,
                 Price = product.Price //doesn't account for sales, would be done here
@@ -66,7 +62,6 @@ public class UserProductController : Controller
             dataContext.Add(order);
             dataContext.SaveChanges();
             return Ok();
-
         }
         catch
         {
@@ -77,59 +72,43 @@ public class UserProductController : Controller
     [HttpPost("sync-cart"), Authorize(Roles = RoleNames.User)]
     public ActionResult<int[]> SyncCart(int[] cart)
     {
- 
-        int? userId = User.GetCurrentUserId();
-        if (userId == null)
-        {
-            return BadRequest();
-        }
 
+        int userId = Convert.ToInt32(User.GetCurrentUserId());
         var products = dataContext.Set<Product>().Where(x => x.Status == Product.StatusType.Active); // filter products that exist
         var userCart = dataContext.Set<CartProduct>().Where(x => x.UserId == userId);
         var userLibrary = dataContext.Set<ProductUser>().Where(x => x.UserId == userId);
         var cartToAdd = new List<int>();
 
-        //don't know a better way, just set everything to remove
         dataContext.RemoveRange(userCart);
 
         foreach (int id in cart)
         {
             if (!products.Any(x => x.Id == id) || userLibrary.Any(x => x.ProductId == id))
-            {
                 continue;
-            }
             dataContext.Add(new CartProduct()
             {
-                UserId = (int)userId,
+                UserId = userId,
                 ProductId = id,
             });
         }
 
         dataContext.SaveChanges();
 
-        foreach(var item in userCart)
+        foreach (var item in userCart)
         {
             cartToAdd.Add(item.ProductId);
         }
 
         return Ok(cartToAdd);
-
     }
 
     [HttpGet("get-cart"), Authorize(Roles = RoleNames.User)]
     public ActionResult<int[]> GetCart()
     {
-        int? userId = User.GetCurrentUserId();
-        if (userId == null)
-        {
-            return BadRequest();
-        }
+        int userId = Convert.ToInt32(User.GetCurrentUserId());
         var userCart = dataContext.Set<CartProduct>().Where(x => x.UserId == userId);
-        var retVal = new List<int>();
-        foreach (var item in userCart)
-        {
-            retVal.Add(item.ProductId);
-        }
+        var retVal = (from item in userCart select item.ProductId).ToList();
+
         return Ok(retVal);
     }
 
