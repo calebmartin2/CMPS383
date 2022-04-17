0e43b9f08cb7305e96a14336ef55e77ada3dcbaf
Caleb Martin <calebm776@cox.net>
2022-04-16 19:37:37 -0500

    Clean FileController

--- SP22.P05.Web/Controllers/FileController.cs
+++ SP22.P05.Web/Controllers/FileController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
 using SP22.P05.Web.Data;
 using SP22.P05.Web.Extensions;
 using SP22.P05.Web.Features.Authorization;
@@ -15,25 +16,21 @@ public class FileController : Controller
     public FileController(DataContext dataContext)
     {
         this.dataContext = dataContext;
-
     }
+
     [HttpPost("updatefile"), Authorize(Roles = RoleNames.AdminOrPublisher)]
     public ActionResult UpdateFile(IFormFile file, [FromForm] int productId)
     {
         var product = dataContext.Set<Product>().First(x => x.Id == productId);
         if (product == null)
-        {
             return BadRequest();
-        }
 
         // delete product file
         //https://stackoverflow.com/questions/1288718/how-to-delete-all-files-and-folders-in-a-directory
         string delPath = Path.Combine(Directory.GetCurrentDirectory(), $"ProductFiles//{productId}//{product.FileName}");
         FileInfo delFile = new FileInfo(delPath);
         if (delFile.Exists)
-        {
             delFile.Delete();
-        }
 
         // attempt to add the new file
         //https://sankhadip.medium.com/how-to-upload-files-in-net-core-web-api-and-react-36a8fbf5c9e8
@@ -60,27 +57,18 @@ public class FileController : Controller
     {
         var product = dataContext.Set<Product>().First(x => x.Id == productId);
         if (product == null)
-        {
             return NotFound("Product not found");
-        }
-        if (product.Status == Product.StatusType.Inactive)
-        {
-            return BadRequest("Product is inactive");
-        }
+        if (User.IsInRole(RoleNames.User) && product.Status == Product.StatusType.Inactive)
+            return Unauthorized("Product is inactive");
         int? userId = User.GetCurrentUserId();
         // Check if user has item in library
         if (User.IsInRole(RoleNames.User) && dataContext.Set<ProductUser>().First(x => x.UserId == userId && x.ProductId == productId) == null)
-        {
             return BadRequest("User does not have item in library");
-        }
-        // Check if publisher has item in library
+        // Check if publisher owns item
         if ((User.IsInRole(RoleNames.Publisher) || User.IsInRole(RoleNames.PendingPublisher)) && dataContext.Set<Product>().First(x => x.PublisherId == userId) == null)
-        {
             return BadRequest("Publisher does not own item");
-        }
 
-        // UNSAFE CODE: user can pass something like ../../ and access files they should not have access to.
-        string path = Path.Combine(Directory.GetCurrentDirectory(), $"ProductFiles//{productId}//", fileName);
+        string path = Path.Combine(Directory.GetCurrentDirectory(), $@"ProductFiles//{productId}//", fileName);
         byte[] bytes = System.IO.File.ReadAllBytes(path);
         return File(bytes, "application/octet-stream", fileName);
     }
@@ -89,7 +77,7 @@ public class FileController : Controller
     public IActionResult DownloadIcon(int productId)
     {
         int CacheAgeSeconds = 60 * 60 * 24; // 1 day
-        Response.Headers["Cache-Control"] = $"public,max-age={CacheAgeSeconds}";
+        Response.Headers[HeaderNames.CacheControl] = $"public,max-age={CacheAgeSeconds}";
 
         try
         {
@@ -109,34 +97,11 @@ public class FileController : Controller
     public FileResult DownloadPicture(int productId, string fileName)
     {
         int CacheAgeSeconds = 60 * 60 * 24; // 1 day
-        Response.Headers["Cache-Control"] = $"public,max-age={CacheAgeSeconds}";
-        // UNSAFE CODE: user can pass something like ../../ and access files they should not have access to.
+        Response.Headers[HeaderNames.CacheControl] = $"public,max-age={CacheAgeSeconds}";
         string path = Path.Combine(Directory.GetCurrentDirectory(), $"ProductFiles//{productId}//Pictures", fileName);
         byte[] bytes = System.IO.File.ReadAllBytes(path);
-        return File(bytes, "application/octet-stream", fileName);
+        return File(bytes, "image/*", fileName);
     }
 
-    [HttpPost("uploadPic/{id}"), Authorize(Roles = RoleNames.AdminOrPublisher)]
-    //https://docs.microsoft.com/en-us/aspnet/core/mvc/models/file-uploads?view=aspnetcore-6.0
-    public async Task<ActionResult> UploadPicturesAsync(List<IFormFile> pictures, int id)
-    {
-
-        foreach (var formFile in pictures)
-        {
-            if (formFile.Length > 0)
-            {
-                string myPath = Path.Combine(Directory.GetCurrentDirectory(), $"ProductFiles//{id}//Pictures");
-                Directory.CreateDirectory(myPath);
-
-                var filePath = Path.Combine(myPath, Guid.NewGuid().ToString() + Path.GetExtension(formFile.FileName));
-
-                using (var stream = System.IO.File.Create(filePath))
-                {
-                    await formFile.CopyToAsync(stream);
-                }
-            }
-        }
-        return Ok();
-    }
 }
 
