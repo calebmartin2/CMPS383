06b50f99cf739c5d31957615dbb3d755f19881d3
Caleb Martin <calebm776@cox.net>
2022-04-21 21:11:44 -0500

    Hide unchanged text if file is selected

--- SP22.P05.Web/ClientApp/src/components/UpdateProductFileModal.js
+++ SP22.P05.Web/ClientApp/src/components/UpdateProductFileModal.js
@@ -2,11 +2,16 @@ import { Button, Form, InputGroup, Modal, Spinner } from "react-bootstrap";
 import { useRef, useState } from "react";
 import axios from "axios";
 
-export function UpdateProductFileModal({product, handleClose}) {
+export function UpdateProductFileModal({ product, handleClose }) {
 
     const fileRef = useRef(null);
     const [addEditLoading, setAddEditLoading] = useState(false);
     const [addProductError, setAddProductError] = useState(false);
+    const [showFileUnchanged, setFileUnchanged] = useState(true);
+
+    function handleUnchangedShow() {
+        fileRef.current && fileRef.current.files.length === 0 ? setFileUnchanged(true) : setFileUnchanged(false)
+    }
 
     const handleUpdateFile = (e) => {
         setAddEditLoading(true);
@@ -31,38 +36,37 @@ export function UpdateProductFileModal({product, handleClose}) {
                 console.log(response);
                 setAddEditLoading(false);
             });
-        }
-            
+    }
 
-    return  (
+    return (
         <>
-        <Modal.Header><Modal.Title>Update File</Modal.Title></Modal.Header>
-        <Modal.Body>
-    <Form onSubmit={handleUpdateFile}>
-        <Form.Group className="mb-4">
-            <Form.Label>File</Form.Label>
-            <InputGroup>
-                <Form.Control required type="file" ref={fileRef}></Form.Control>
-            </InputGroup>
-        </Form.Group>
-        <Modal.Footer>
-            <Button className="custom-primary-btn" variant="primary" type="submit" disabled={addEditLoading}>
-            {addEditLoading ? <><Spinner
-                        as="span"
-                        animation="border"
-                        size="sm"
-                        role="status"
-                        aria-hidden="true"
-                    /> Updating File </> : 'Update File'}
-            </Button>
-            <Button variant="danger" onClick={handleClose}>
-                Discard
-            </Button>
-        </Modal.Footer>
-        {addProductError && <p style={{ marginTop: "1em", background: "#500000", padding: "1em" }}>Invalid Submission</p>}
-    </Form>
-    </Modal.Body>
+            <Modal.Header><Modal.Title>Update File</Modal.Title></Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleUpdateFile}>
+                    <Form.Group className="mb-4">
+                        <Form.Label>File {showFileUnchanged && "(unchanged)"}</Form.Label>
+                        <InputGroup>
+                            <Form.Control required type="file" ref={fileRef} onInput={handleUnchangedShow}></Form.Control>
+                        </InputGroup>
+                    </Form.Group>
+                    <Modal.Footer>
+                        <Button className="custom-primary-btn" variant="primary" type="submit" disabled={addEditLoading}>
+                            {addEditLoading ? <><Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            /> Updating File </> : 'Update File'}
+                        </Button>
+                        <Button variant="danger" onClick={handleClose}>
+                            Discard
+                        </Button>
+                    </Modal.Footer>
+                    {addProductError && <p style={{ marginTop: "1em", background: "#500000", padding: "1em" }}>Invalid Submission</p>}
+                </Form>
+            </Modal.Body>
 
-    </>
+        </>
     )
 }
