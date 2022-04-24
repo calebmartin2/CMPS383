c3f5851128500065cd935b4ecbc416ba144f4e22
Caleb Martin <calebm776@cox.net>
2022-04-24 10:12:55 -0500

    remove useEffect import

--- SP22.P05.Web/ClientApp/src/pages/Store/Search.js
+++ SP22.P05.Web/ClientApp/src/pages/Store/Search.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Breadcrumb, Button, Col, Form, FormControl, Row } from "react-bootstrap";
 import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { ProductList } from "./ProductList";
